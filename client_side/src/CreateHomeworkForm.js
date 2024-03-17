b553197cf320015434f0c4f3cfdab43bff1dc1e9
Mikhail Shishov <63235817+FANDIM-wolf@users.noreply.github.com>
2024-03-17 12:31:52 +0300

    Add files via upload

--- client_side/src/CreateHomeworkForm.js
+++ client_side/src/CreateHomeworkForm.js
@@ -1,38 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
+import './App.css'; // Import the CSS file
 
-const FileUploadComponent = () => {
-    const [selectedFile, setSelectedFile] = useState(null);
+//Компонент отрисовывает форму для создания домашнего задания
+const CreateHomeworkForm = () => {
     const [cookies] = useCookies(['vkid']);
+    const [group, setGroup] = useState('');
+    const [subject, setSubject] = useState('');
+    const [description, setDescription] = useState('');
 
-    const handleFileChange = (event) => {
-        setSelectedFile(event.target.files[0]);
-    };
-    // Функция для загрузки документа 
-    const handleUpload = () => {
-        const formData = new FormData();
-        formData.append('file', selectedFile);
+    const vkid = cookies.vkid || '';
 
-        axios.post('http://localhost:8000/api/upload_file_and_save/', formData, {
-            headers: {
-                'Cookie': `vkid=${cookies.vkid}`  
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:8000/api/create_homework/', {
+                vkid,
+                group_name: group,
+                subject_name: subject,
+                description,
+            });
+
+            if (response.status === 200) {
+                alert('Homework created successfully');
+               
+            } else {
+                alert('Failed to create homework');
             }
-        })
-        .then(response => {
-            console.log(response.data);
-        })
-        .catch(error => {
-            console.error('Ошибка:', error);
-        });
+        } catch (error) {
+            console.error('Error creating homework:', error);
+            alert('An error occurred while creating homework');
+        }
     };
 
     return (
-        <div className='_edit_container'>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleUpload}>Загрузить файл</button>
-        </div>
+        <form onSubmit={handleSubmit} className="formContainer">
+            <input type="text" value={vkid} readOnly className="inputField" placeholder="VK ID" />
+            <input type="text" value={group} onChange={(e) => setGroup(e.target.value)} className="inputField" placeholder="Group Name" />
+            <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} className="inputField" placeholder="Subject Name" />
+            <textarea value={description} onChange={(e) => setDescription(e.target.value)} className="textareaField" placeholder="Description"></textarea>
+            <button type="submit" className="button">Создать Задание</button>
+        </form>
     );
 };
 
-export default FileUploadComponent;
+export default CreateHomeworkForm;
